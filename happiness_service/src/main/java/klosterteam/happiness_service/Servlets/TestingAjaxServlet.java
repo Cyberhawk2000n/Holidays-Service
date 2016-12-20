9c0d12963d4d0eb4a32e2860e9a183587d62d8e8
Lepec1n <stas33553377@yandex.ru>
2016-12-20 17:51:23 +0300

    Added file upload example. -> TestingAjaxServlet || testing-ajax.html

--- happiness_service/src/main/java/klosterteam/happiness_service/Servlets/TestingAjaxServlet.java
+++ happiness_service/src/main/java/klosterteam/happiness_service/Servlets/TestingAjaxServlet.java
@@ -1,14 +1,28 @@
 package klosterteam.happiness_service.Servlets;
 
 import java.io.*;
+import java.util.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.output.*;
+
 import javax.json.Json;
 import javax.json.JsonObject;
 
 public class TestingAjaxServlet extends HttpServlet {
+
+    private boolean isMultipart;
+    private String filePath = "D:\\Servers\\data\\";
+    private int maxFileSize = 50 * 1024;
+    private int maxMemSize = 4 * 1024;
+    private File file ;
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -30,7 +44,79 @@ public class TestingAjaxServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        if(request.getParameter("message") != null)
+            processRequest(request, response);
+
+        // Check that we have a file upload request
+        isMultipart = ServletFileUpload.isMultipartContent(request);
+        response.setContentType("text/html");
+        java.io.PrintWriter out = response.getWriter( );
+        if( !isMultipart ){
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet upload</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<p>No file uploaded</p>");
+            out.println("</body>");
+            out.println("</html>");
+            return;
+        }
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        // maximum size that will be stored in memory
+        factory.setSizeThreshold(maxMemSize);
+        // Location to save data that is larger than maxMemSize.
+        factory.setRepository(new File("D:\\Servers\\data"));
+
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        // maximum file size to be uploaded.
+        upload.setSizeMax( maxFileSize );
+
+        try{
+            // Parse the request to get file items.
+            List fileItems = upload.parseRequest(request);
+
+            // Process the uploaded file items
+            Iterator i = fileItems.iterator();
+
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet upload</title>");
+            out.println("</head>");
+            out.println("<body>");
+            while ( i.hasNext () )
+            {
+                FileItem fi = (FileItem)i.next();
+                if ( !fi.isFormField () )
+                {
+                    // Get the uploaded file parameters
+                    String fieldName = fi.getFieldName();
+                    String fileName = fi.getName();
+                    String contentType = fi.getContentType();
+                    boolean isInMemory = fi.isInMemory();
+                    long sizeInBytes = fi.getSize();
+                    // Write the file
+                    if( fileName.lastIndexOf("\\") >= 0 ){
+                        file = new File( filePath +
+                                fileName.substring( fileName.lastIndexOf("\\"))) ;
+                    }else{
+                        file = new File( filePath +
+                                fileName.substring(fileName.lastIndexOf("\\")+1)) ;
+                    }
+                    System.out.println("file Name: "+fileName);
+                    System.out.println("file path: "+filePath);
+                    System.out.println("fi: "+fi.toString());
+                    System.out.println("FILE INFO. \n PATH: "+ file.getPath() + "\n Name: "+file.getName()+"\n Total Space: "+file.getTotalSpace());
+                    fi.write( file ) ;
+                    out.println("Uploaded Filename: " + fileName + "<br>");
+                }
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }catch(Exception ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
